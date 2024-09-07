7731e63d8199960b49e8b6d31fb73320387fce91
customer-manager <cmanager747@gmail.com>
2024-09-07 18:34:35 +0300

    added mal render

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,14 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './reducers/AuthReducer';
-import PostReducer from './reducers/PostReducer';
-import ProfileReducer from './reducers/ProfileReducer';
-import fetchedPostReducer from './reducers/FetchedPostReducer';
 
 const rootReducer = combineReducers({
   auth:authReducer,
-  post:PostReducer,
-  fetchedPosts:fetchedPostReducer,
-  profile:ProfileReducer
 });
 
 const store = configureStore({
